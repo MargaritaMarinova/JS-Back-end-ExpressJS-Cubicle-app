02ed8160acfd157cec6dad7e4454885791f921d8
MargaritaMarinova <margarita.tomova@gmail.com>
2020-06-23 15:41:27 +0300

    feat: update handlebars templates and add new ones

--- controllers/create-cube.js
+++ controllers/create-cube.js
@@ -1,5 +1,10 @@
 const Cube = require('../models/cube')
 
-const newCube = new Cube('First', 'First test for cube', 'https://google.com', 1)
+/*const newCube = new Cube('Gan356 Air SM', 'test1 for cube', 'https://ae01.alicdn.com/kf/HTB1CSddXRxRMKJjy0Fdq6yifFXa6/Gan-356-Air-SM-3x3-Black-Magic-cube-GAN-Air-SM-Magnetic-3x3x3-Speed-cube-gans.jpg', 3)
+const newCube2 = new Cube('Eco-Dark', 'test2 for cube', 'https://thingsidesire.com/wp-content/uploads/2018/06/Eco-Dark-Rubik%E2%80%99s-Cube2.jpg', 6)
+const newCube3 = new Cube('Pyraminx', 'test3 for cube', 'https://images-na.ssl-images-amazon.com/images/I/61izOzq%2BBAL._SY355_.jpg', 1)
+const newCube4 = new Cube('Megaminx', 'test4 for cube', 'https://images-na.ssl-images-amazon.com/images/I/61HpQqVQ37L._SY355_.jpg', 3)
 
-newCube.save()
+
+newCube4.save()*/
+
